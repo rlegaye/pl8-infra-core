fc9f9011d8e15f4fbf781d5355748a5a5d093841
Robert LeGaye <robertlegaye@Roberts-MacBook-Pro.local>
2022-12-12 22:15:54 -0800

    mysql versions

--- versions.tf
+++ versions.tf
@@ -1,15 +1,10 @@
 terraform {
+  required_version = ">= 1.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
+      version = ">= 4.45"
     }
   }
-
-  required_version = ">= 0.14.0"
 }
