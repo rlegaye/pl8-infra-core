54b8be0f25225ef8a34d531bce64807649a766ea
Robert LeGaye <robertlegaye@Roberts-MacBook-Pro.local>
2022-12-19 14:46:18 -0800

    mysql

--- outputs.tf
+++ outputs.tf
@@ -42,4 +42,5 @@ output "autoscaling_capacity_providers" {
 output "cluster_master_usernamez" {
   description = "rds master username"
   value       = module.rds_pl8_core.cluster_master_username
+  sensitive   = true
 }
