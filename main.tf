296dcff6bfe6dc22f7ac0ac1892fecf7251846a8
Robert LeGaye <robertlegaye@Roberts-MacBook-Pro.local>
2022-12-19 16:00:28 -0800

    mysql

--- main.tf
+++ main.tf
@@ -185,9 +185,12 @@ module "vpc" {
   name = local.name
   cidr = "10.99.0.0/18"
 
-  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  public_subnets  = ["10.99.0.0/24", "10.99.1.0/24", "10.99.2.0/24"]
-  private_subnets = ["10.99.3.0/24", "10.99.4.0/24", "10.99.5.0/24"]
+  azs             = ["${local.region}a"]
+  public_subnets  = ["10.99.0.0/24"]
+  private_subnets = ["10.99.3.0/24"]
+#  azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
+#  public_subnets  = ["10.99.0.0/24", "10.99.1.0/24", "10.99.2.0/24"]
+#  private_subnets = ["10.99.3.0/24", "10.99.4.0/24", "10.99.5.0/24"]
 
   enable_nat_gateway      = true
   single_nat_gateway      = true
