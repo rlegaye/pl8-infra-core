6b840cc544cc74379d074929964114945b16aaca
Robert LeGaye <robertlegaye@Roberts-MacBook-Pro.local>
2023-01-18 20:31:19 -0800

    multi az

--- rds-pl8-core/main.tf
+++ rds-pl8-core/main.tf
@@ -160,10 +160,10 @@ module "vpc" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  azs              = ["${local.region}a"]
-  public_subnets   = ["10.99.0.0/24"]
-  private_subnets  = ["10.99.3.0/24"]
-  database_subnets = ["10.99.7.0/24"]
+  azs              = ["${local.region}a", "${local.region}b"]
+  public_subnets   = ["10.99.0.0/24", "10.99.1.0/24"]
+  private_subnets  = ["10.99.3.0/24", "10.99.4.0/24"]
+  database_subnets = ["10.99.7.0/24", "10.99.8.0/24"]
 
 #  azs              = ["${local.region}a", "${local.region}b", "${local.region}c"]
 #  public_subnets   = ["10.99.0.0/24", "10.99.1.0/24", "10.99.2.0/24"]
